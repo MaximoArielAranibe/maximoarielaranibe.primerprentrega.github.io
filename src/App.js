66e8d5ed8fc3dfe49cd54a304c22e01a5c84c372
Maximo Aranibe <darknesswong@gmail.com>
2023-07-10 19:43:25 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './Components/NavBar/NavBar.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+    </div>
+  );
+}
+
+export default App;
