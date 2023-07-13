a7a027d82836c52734eb00ce107251cd61cfa437
Maximo Aranibe <darknesswong@gmail.com>
2023-07-13 18:34:29 -0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import NavBar from './Components/NavBar/NavBar.jsx';
 import './App.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListcontainer.jsx';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <ItemListContainer>
+        
+      </ItemListContainer>
     </div>
   );
 }
